1ed92ff5328d58b6889dffd03cdf624b8315d7ec
davidmuir1999 <david.bme.muir2017@gmail.com>
2023-03-22 13:54:41 +0000

    Nav Bar with Animation

---
+++ src/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Homescreen.css";
+import Nav from './Nav';
+
+function HomeScreen() {
+  return (
+    <div className='homescreen'>
+      <Nav/>
+    </div>
+  )
+}
+
+export default HomeScreen
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD9t8cl8vfc_SaFQlYWPVCufNeCAJZ70js",
+    authDomain: "netflix-clone-4ffd8.firebaseapp.com",
+    projectId: "netflix-clone-4ffd8",
+    storageBucket: "netflix-clone-4ffd8.appspot.com",
+    messagingSenderId: "473328097633",
+    appId: "1:473328097633:web:6a2e185aa3546001fccd20"
+  };
