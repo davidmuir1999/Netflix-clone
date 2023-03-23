61743d295549899b0feac68eb94f4d4651c6140c
davidmuir1999 <david.bme.muir2017@gmail.com>
2023-03-23 20:12:32 +0000

    Movie API Integration Banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Banner.css";
+import axios from './axios';
+import requests from './requests';
 
 function Banner() {
+
+    const [movie,setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData(){
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length -1)
+                ]
+            );
+            return request;
+        }
+
+        fetchData();
+    }, []);
+
+    function truncate(string, n){
+        return string?.length > n ? string.substr(0, n-1) + '...' : string;
+    }
+
     return (
         <header className='banner'
             style={{
+                backgroundImage: `url("https://image.tmdb/org/t/p/original/${movie?.backdrop_path}")`,
                 backgroundSize: "cover",
                 backgroundPosition: "center center",
+                
             }}>
 
             <div className='banner__contents'>
@@ -16,7 +41,7 @@ function Banner() {
                         <button className='banner__btn'>My List</button>
                     </div>
                     <h1 className='banner__description'>
-                        Test
+                        {truncate(``, 150)}
                     </h1>
             </div>
             <div className='banner--fadeBottom'/>
