0a5ad29ba4b992d8a969110cbecf9dab99c319a8
davidmuir1999 <david.bme.muir2017@gmail.com>
2023-03-27 21:19:08 +0100

    API fetch banner display

--- src/Banner.js
+++ src/Banner.js
@@ -21,6 +21,8 @@ function Banner() {
         fetchData();
     }, []);
 
+    console.log(movie);
+
     function truncate(string, n){
         return string?.length > n ? string.substr(0, n-1) + '...' : string;
     }
@@ -28,12 +30,10 @@ function Banner() {
     return (
         <header className='banner'
             style={{
-                backgroundImage: `url("https://image.tmdb/org/t/p/original/${movie?.backdrop_path}")`,
-                backgroundSize: "cover",
-                backgroundPosition: "center center",
-                
-            }}>
-
+                backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`
+                // backgroundImage: `url("https://cdn.pixabay.com/photo/2015/08/23/09/22/banner-902589__340.jpg")`
+            }}
+            >
             <div className='banner__contents'>
                 <h1 className='banner__title'> Movie Name</h1>
                     <div className='banner__btns'>
