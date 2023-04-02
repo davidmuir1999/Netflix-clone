a47f77c182d2f37c87d7198fa6c26dac5fc076d8
davidmuir1999 <david.bme.muir2017@gmail.com>
2023-04-02 16:10:45 +0100

    Authenticator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import HomeScreen from './screens/HomeScreen';
-import Loginscreen from './screens/Loginscreen';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { login, logout, selectUser } from "./features/userSlice";
+import { auth } from "./firebase";
+import HomeScreen from "./screens/HomeScreen";
+import Loginscreen from "./screens/Loginscreen";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
-  const user = null;
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        dispatch(logout);
+      }
+    });
+    return unsubscribe;
+  }, []);
 
   return (
     <div className="app">
@@ -15,7 +34,7 @@ function App() {
           <Loginscreen />
         ) : (
           <Routes>
-            <Route exact path='/' element={<HomeScreen />} />
+            <Route exact path="/" element={<HomeScreen />} />
           </Routes>
         )}
       </Router>
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyD9t8cl8vfc_SaFQlYWPVCufNeCAJZ70js",
-    authDomain: "netflix-clone-4ffd8.firebaseapp.com",
-    projectId: "netflix-clone-4ffd8",
-    storageBucket: "netflix-clone-4ffd8.appspot.com",
-    messagingSenderId: "473328097633",
-    appId: "1:473328097633:web:6a2e185aa3546001fccd20"
-  };
+  apiKey: "AIzaSyD9t8cl8vfc_SaFQlYWPVCufNeCAJZ70js",
+  authDomain: "netflix-clone-4ffd8.firebaseapp.com",
+  projectId: "netflix-clone-4ffd8",
+  storageBucket: "netflix-clone-4ffd8.appspot.com",
+  messagingSenderId: "473328097633",
+  appId: "1:473328097633:web:6a2e185aa3546001fccd20",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword };
+export default db;
